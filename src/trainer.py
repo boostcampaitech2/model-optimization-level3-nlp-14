47188804fb69b45bebff05fc62bf82ee05088360
Amber-Chaeeunk <chaechae1214@naver.com>
2021-12-04 10:27:33 +0000

    [#4] Add distillation trainer and backbone trainer to trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -76,8 +76,202 @@ def _get_len_label_from_dataset(dataset: Dataset) -> int:
     else:
         raise NotImplementedError
 
-
 class TorchTrainer:
+    """Pytorch Trainer."""
+
+    def __init__(
+        self,
+        model: nn.Module,
+        criterion: nn.Module,
+        optimizer: optim.Optimizer,
+        scheduler,
+        model_path: str,
+        scaler=None,
+        device: torch.device = "cpu",
+        verbose: int = 1,
+    ) -> None:
+        """Initialize TorchTrainer class.
+        Args:
+            model: model to train
+            criterion: loss function module
+            optimizer: optimization module
+            device: torch device
+            verbose: verbosity level.
+        """
+
+        self.model = model
+        self.model_path = model_path
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.scaler = scaler
+        self.verbose = verbose
+        self.device = device
+        self.early_stopping = EarlyStopping(self.model_path, patience = 1000, delta=0)
+
+    def train(
+        self,
+        train_dataloader: DataLoader,
+        n_epoch: int,
+        val_dataloader: Optional[DataLoader] = None,
+        wandb_log=False,
+    ) -> Tuple[float, float]:
+        """Train model.
+        Args:
+            train_dataloader: data loader module which is a iterator that returns (data, labels)
+            n_epoch: number of total epochs for training
+            val_dataloader: dataloader for validation
+        Returns:
+            loss and accuracy
+        """
+        best_test_acc = -1.0
+        best_test_f1 = -1.0
+        num_classes = _get_len_label_from_dataset(train_dataloader.dataset)
+        label_list = [i for i in range(num_classes)]
+
+        # Run training and track with wandb
+        total_batches = len(train_dataloader) * n_epoch
+        example_ct = 0  # number of examples seen
+        batch_ct = 0
+        
+        for epoch in range(n_epoch):
+            running_loss, correct, total = 0.0, 0, 0
+            preds, gt = [], []
+            pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
+            self.model.train()
+            for batch, (data, labels) in pbar:
+                data, labels = data.to(self.device), labels.to(self.device)
+
+                if self.scaler:
+                    with torch.cuda.amp.autocast():
+                        outputs = self.model(data)
+                else:
+                    outputs = self.model(data)
+                outputs = torch.squeeze(outputs)
+                loss = self.criterion(outputs, labels)
+
+                self.optimizer.zero_grad()
+
+                if self.scaler:
+                    self.scaler.scale(loss).backward()
+                    self.scaler.step(self.optimizer)
+                    self.scaler.update()
+                else:
+                    loss.backward()
+                    self.optimizer.step()
+                self.scheduler.step()
+
+                _, pred = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (pred == labels).sum().item()
+                preds += pred.to("cpu").tolist()
+                gt += labels.to("cpu").tolist()
+
+                running_loss += loss.item()
+                pbar.update()
+                pbar.set_description(
+                    f"Train: [{epoch + 1:03d}] "
+                    f"Loss: {(running_loss / (batch + 1)):.3f}, "
+                    f"Acc: {(correct / total) * 100:.2f}% "
+                    f"F1(macro): {f1_score(y_true=gt, y_pred=preds, labels=label_list, average='macro', zero_division=0):.2f}"
+                )
+
+                example_ct += len(labels)
+                batch_ct += 1
+                if wandb_log and ((batch_ct + 1) % 100) == 0:
+                    wandb.log(
+                        {
+                            "epoch": epoch,
+                            "loss" : running_loss / (batch + 1),
+                            "acc" : correct / total,
+                            "F1(macro)" : f1_score(y_true=gt, y_pred=preds, labels=label_list, average='macro', zero_division=0)
+                        }, step=example_ct)
+                    # print(f"Loss after " + str(example_ct).zfill(5) + f" examples: {loss:.3f}")
+            
+            pbar.close()
+
+            val_loss, test_f1, test_acc = self.test(
+                model=self.model, test_dataloader=val_dataloader
+            )
+            if wandb_log:
+                wandb.log(
+                    {
+                        "eval_loss" : val_loss,
+                        "eval_acc" : test_acc,
+                        "eval_F1(macro)" : test_f1
+                    }, step=example_ct)
+        
+            if best_test_f1 > test_f1:
+                continue
+            best_test_acc = test_acc
+            best_test_f1 = test_f1
+            print(f"Model saved. Current best test f1: {best_test_f1:.3f}")
+            self.early_stopping(val_loss, self.model, self.optimizer, self.scheduler)
+            if self.early_stopping.early_stop:
+                print("Early Stopping")
+                return best_test_acc, best_test_f1
+
+        return best_test_acc, best_test_f1
+
+
+
+    @torch.no_grad()
+    def test(
+        self, model: nn.Module, test_dataloader: DataLoader
+    ) -> Tuple[float, float, float]:
+        """Test model.
+        Args:
+            test_dataloader: test data loader module which is a iterator that returns (data, labels)
+        Returns:
+            loss, f1, accuracy
+        """
+
+        n_batch = _get_n_batch_from_dataloader(test_dataloader)
+
+        running_loss = 0.0
+        preds = []
+        gt = []
+        correct = 0
+        total = 0
+
+        num_classes = _get_len_label_from_dataset(test_dataloader.dataset)
+        label_list = [i for i in range(num_classes)]
+
+        pbar = tqdm(enumerate(test_dataloader), total=len(test_dataloader))
+        model.to(self.device)
+        model.eval()
+        for batch, (data, labels) in pbar:
+            data, labels = data.to(self.device), labels.to(self.device)
+
+            if self.scaler:
+                with torch.cuda.amp.autocast():
+                    outputs = model(data)
+            else:
+                outputs = model(data)
+            outputs = torch.squeeze(outputs)
+            running_loss += self.criterion(outputs, labels).item()
+
+            _, pred = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (pred == labels).sum().item()
+
+            preds += pred.to("cpu").tolist()
+            gt += labels.to("cpu").tolist()
+            pbar.update()
+            pbar.set_description(
+                f" Val: {'':5} Loss: {(running_loss / (batch + 1)):.3f}, "
+                f"Acc: {(correct / total) * 100:.2f}% "
+                f"F1(macro): {f1_score(y_true=gt, y_pred=preds, labels=label_list, average='macro', zero_division=0):.2f}"
+            )
+        loss = running_loss / len(test_dataloader)
+        accuracy = correct / total
+        f1 = f1_score(
+            y_true=gt, y_pred=preds, labels=label_list, average="macro", zero_division=0
+        )
+        return loss, f1, accuracy
+
+
+class KD_TorchTrainer:
     """Pytorch Trainer."""
 
     def __init__(
@@ -261,7 +455,6 @@ class TorchTrainer:
             else:
                 outputs = student_model(data)
             outputs = torch.squeeze(outputs)
-            # running_loss += self.criterion(outputs, labels).item()
             running_loss += F.cross_entropy(input=outputs, target=labels).item()
             _, pred = torch.max(outputs, 1)
             total += labels.size(0)
